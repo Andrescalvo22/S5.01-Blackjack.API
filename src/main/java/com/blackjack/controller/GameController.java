ae72f0a5ebb79ff30e92abe42e4f80a66078b331
Andres <andrescalvo01@gmail.com>
2025-11-16 13:12:16 +0100

    feat: Creating methods in GameController to work with the EndPoints

--- src/main/java/com/blackjack/controller/GameController.java
+++ src/main/java/com/blackjack/controller/GameController.java
@@ -1,4 +1,55 @@
 package com.blackjack.controller;
 
+import com.blackjack.model.Hand;
+import com.blackjack.service.GameService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/game")
 public class GameController {
+
+    private final GameService gameService;
+
+    public GameController() {
+        this.gameService = new GameService();
+    }
+
+    @PostMapping("/start")
+    public String startGame() {
+        gameService.startNewGame();
+        return "New Game Started!";
+    }
+
+    @PostMapping("/hit")
+    public String playerHit() {
+        if (gameService.isGameOver()) {
+            return "Game is Over! Start a new Game.";
+        }
+        gameService.playerHit();
+        if (gameService.isGameOver()) {
+            return "Player lost, dealer wins!";
+        }
+        return "Card drawn, player hand value: " + gameService.getPlayerHand().calculateValue();
+    }
+
+    @PostMapping("/stand")
+    public String playerStand() {
+        if (gameService.isGameOver()) {
+            return "Game is Over! Start a new Game.";
+        }
+        gameService.playerStand();
+        String winner = gameService.determineWinner();
+        return "Game Over! " + winner;
+    }
+
+    @GetMapping("/status")
+    public String gameStatus() {
+        Hand playerHand = gameService.getPlayerHand();
+        Hand dealerHand = gameService.getDealerHand();
+        return "Player Hand: " + playerHand.getCards() + "(value: " + playerHand.calculateValue() + ") \n" +
+                "Dealer Hand: " + dealerHand.getCards() + "(value: " + dealerHand.calculateValue() + ") \n" +
+                "Game Over: " + gameService.isGameOver();
+    }
+
 }
+
