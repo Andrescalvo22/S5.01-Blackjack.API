3009e524943f9ba2bbd7d9b30f1367a5d52bb8a3
Andres <andrescalvo01@gmail.com>
2025-11-19 19:10:57 +0100

    feat: Creating GameStartDTO that has the lists of cards that the player and dealer have at the beginning

---
+++ src/main/java/com/blackjack/dto/GameStartDTO.java
@@ -0,0 +1,16 @@
+package com.blackjack.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class GameStartDTO {
+    private String gameId;
+    private String playerId;
+
+    private List<CardDTO> playerCards;
+    private List<CardDTO> dealerCards;
+}
