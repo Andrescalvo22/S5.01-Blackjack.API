a0a7d69043a9202f6d0411c6508c425e2f690e21
Andres <andrescalvo01@gmail.com>
2025-11-20 13:04:38 +0100

    feat: Creating PlayerEntity to use this model in SQL DataBase

---
+++ src/main/java/com/blackjack/sqlmodel/PlayerEntity.java
@@ -0,0 +1,24 @@
+package com.blackjack.sqlmodel;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "players")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PlayerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private int score;
+
+    private int gamesPlayed;
+}
