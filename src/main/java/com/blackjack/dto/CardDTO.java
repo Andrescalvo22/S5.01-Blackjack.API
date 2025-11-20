5820fa7e637ba9b6688d5708b416c68cd06136b8
Andres <andrescalvo01@gmail.com>
2025-11-20 14:10:05 +0100

    chore: Adding AllArgsConstructor to the DTO

--- src/main/java/com/blackjack/dto/CardDTO.java
+++ src/main/java/com/blackjack/dto/CardDTO.java
@@ -1,9 +1,13 @@
 package com.blackjack.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CardDTO {
     private String rank;
