8a58b302a00ff48b93c9d12dc19c31cb25d6381d
Andres <andrescalvo01@gmail.com>
2025-11-20 12:42:39 +0100

    chore: Adapting the GameController to use the Dtos

--- src/main/java/com/blackjack/controller/GameController.java
+++ src/main/java/com/blackjack/controller/GameController.java
@@ -1,6 +1,9 @@
 package com.blackjack.controller;
 
-import com.blackjack.model.Game;
+import com.blackjack.dto.ActionResponseDTO;
+import com.blackjack.dto.GameStartDTO;
+import com.blackjack.dto.GameStatusDTO;
+import com.blackjack.mapper.DtoMapper;
 import com.blackjack.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,24 +17,29 @@ public class GameController {
     private final GameService gameService;
 
     @PostMapping("/start/{playerId}")
-    public Mono<Game> startGame(@PathVariable String playerId) {
-        return gameService.startGame(playerId);
+    public Mono<GameStartDTO> startGame(@PathVariable String playerId) {
+        return gameService.startGame(playerId)
+                .map(DtoMapper::toGameStartDTO);
     }
 
     @GetMapping("/{id}")
-    public Mono<Game> getGame(@PathVariable String id) {
-        return gameService.getGame(id);
+    public Mono<GameStatusDTO> getGame(@PathVariable String id) {
+        return gameService.getGame(id)
+                .map(DtoMapper::toGameStatusDTO);
     }
 
     @PostMapping("/{id}/hit")
-    public Mono<Game> hit(@PathVariable String id) {
-        return gameService.hit(id);
+    public Mono<ActionResponseDTO> hit(@PathVariable String id) {
+        return gameService.hit(id)
+                .map(game -> DtoMapper.toActionResponseDTO("Card drawn", game));
     }
 
     @PostMapping("/{id}/stand")
-    public Mono<Game> stand(@PathVariable String id) {
-        return gameService.stand(id);
+    public Mono<ActionResponseDTO> stand(@PathVariable String id) {
+        return gameService.stand(id)
+                .map(game -> DtoMapper.toActionResponseDTO("Stand complete", game));
     }
 }
+
 
 
